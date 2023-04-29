936f792437c49d0ac9640a49cec3b684abdfb813
RedUmbrella <trythonminecraft@gmail.com>
2023-04-30 00:52:04 +0530

    compliments ot eth fech rof eth azzip

--- pizza.py
+++ pizza.py
@@ -1,10 +1,13 @@
+# List of possible options
+# Could be made into a dictionary later
 top = ["Pepperoni", "Sausage", "Olives", "Bell pepper", "Paneer"]
 crust = ["Thin Crust", "Neapolitian Style", "New York Style"]
 sauce = ["Marinara", "White", "Pesto", "Barbequeue"]
 cheese = ["Mozerella", "Cheddar", "Provolone", "Pecorino-Romano"]
 
 
-def listItemSelect(l1):
+# Prints all items in a numbered list and asks you to choose a value according to the numbers
+def listItemSelect(l1: list):
     for i in range(len(l1)):
         print(i + 1, l1[i])
     while True:
@@ -17,10 +20,8 @@ def listItemSelect(l1):
     return val
 
 
-pizzeriaIsOpen = True
-
-
-def multilIS(n, l1):
+# Returns a variable or a list depending on how many I need
+def multiLIS(n: int, l1: list):
     if n == 1:
         val = listItemSelect(l1)
     else:
@@ -32,8 +33,12 @@ def multilIS(n, l1):
     return val
 
 
+pizzeriaIsOpen = True
+
 while pizzeriaIsOpen:
-    print("MAKE YOUR OWN PIZZA NIGGER")
+    print("MAKE YOUR OWN PIZZA nig—")
+
+    # Set up for multiLIS with numbers and lists
     n = int(input("How many toppings"))
     pizza = {
         "Toppings": [n, top],
@@ -43,8 +48,7 @@ while pizzeriaIsOpen:
     }
 
     for i, j in pizza.items():
-        pizza[i] = multilIS(j[0], j[1])
-        print(pizza)
+        pizza[i] = multiLIS(j[0], j[1])
 
     query = input("Quit? (Y/n)")
     if query in "Nn":
